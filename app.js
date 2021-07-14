1bb206b43e28e87ccd7ad3fdf342d22495b9d6f7
pawel.szczepankiewicz <pawel.szczepankiewicz@enginiety.com>
2021-07-14 15:54:48 +0200

    Added multiple functionalities

--- app.js
+++ app.js
@@ -1,29 +1,50 @@
-const express = require('express');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const flash = require('connect-flash');
-const app = express();
+const express = require('express')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const flash = require('connect-flash')
+const markdown = require('marked')
+const app = express()
+const sanitizeHTML = require('sanitize-html')
 
 let sessionOptions = session({
     secret: "Something no one should guess!",
-    store: MongoStore.create({client: require('./db')}),
+    store: MongoStore.create({ client: require('./db') }),
     resave: false,
     saveUninitialized: false,
-    cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
-});
+    cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true }
+})
 
-app.use(sessionOptions);
-app.use(flash());
+app.use(sessionOptions)
+app.use(flash())
 
-const router = require('./router');
+//Allows for every tample to have logged in user info
+app.use(function (req, res, next) {
+    //Make markdown function available from within ejs templates
+    res.locals.filterUserHtml = function (content) {
+        return sanitizeHTML(markdown(content), {allowedTags: ['p', 'br', 'ul', 'li', 'ol', 'strong', 'bold', 'i', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'], allowedAttributes: {}})
+    }
 
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+    // make all error and success flash messages available from all templates
+    res.locals.errors = req.flash("errors")
+    res.locals.success = req.flash("success")
 
-app.use(express.static('public'));
-app.set('views', 'views');
-app.set('view engine', 'ejs');
+    // make current user id available on the req object
+    if (req.session.user) { req.visitorId = req.session.user._id } else { req.visitorId = 0 }
 
-app.use('/', router);
+    // make user session data available from within view templates
+    res.locals.user = req.session.user
+    next()
+})
 
-module.exports = app;
+const router = require('./router')
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use(express.static('public'))
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
+app.use('/', router)
+
+module.exports = app
--- models/User.js
+++ models/User.js
@@ -1,10 +1,13 @@
 const bcrypt = require("bcryptjs")
 const usersCollection = require('../db').db().collection("users")
 const validator = require("validator")
+const md5 = require('md5')
 
-let User = function (data) {
+let User = function (data, getAvatar) {
     this.data = data
     this.errors = []
+    if (getAvatar == undefined) { getAvatar = false }
+    if (getAvatar) { this.getAvatar() }
 }
 
 User.prototype.cleanUp = function () {
@@ -26,7 +29,7 @@ User.prototype.validate = function () {
         if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) { this.errors.push("Username can only contain letters and numbers.") }
         if (!validator.isEmail(this.data.email)) { this.errors.push("You must provide a valid email address.") }
         if (this.data.password == "") { this.errors.push("You must provide a password.") }
-        if (this.data.password.length > 0 && this.data.password.length < 12) { this.errors.push("Password must be at least 12 characters.") }
+        if (this.data.password.length > 0 && this.data.password.length < 6) { this.errors.push("Password must be at least 6 characters.") }
         if (this.data.password.length > 50) { this.errors.push("Password cannot exceed 50 characters.") }
         if (this.data.username.length > 0 && this.data.username.length < 3) { this.errors.push("Username must be at least 3 characters.") }
         if (this.data.username.length > 30) { this.errors.push("Username cannot exceed 30 characters.") }
@@ -51,6 +54,8 @@ User.prototype.login = function () {
         this.cleanUp()
         usersCollection.findOne({ username: this.data.username }).then((attemptedUser) => {
             if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
+                this.data = attemptedUser
+                this.getAvatar()
                 resolve("Congrats!")
             } else {
                 reject("Invalid username / password.")
@@ -74,10 +79,39 @@ User.prototype.register = function () {
             let salt = bcrypt.genSaltSync(10)
             this.data.password = bcrypt.hashSync(this.data.password, salt)
             await usersCollection.insertOne(this.data)
+            this.getAvatar()
             resolve()
         } else {
             reject(this.errors)
         }
+    })
+}
+
+User.prototype.getAvatar = function () {
+    this.avatar = `https://gravatar.com/avatar/${md5(this.data.email)}?s=128`
+}
+
+User.findByUsername = function (username) {
+    return new Promise(function (resolve, reject) {
+        if (typeof(username) != "string") {
+            reject()
+            return
+        }
+        usersCollection.findOne({username: username}).then(function (userDoc) {
+            if (userDoc) {
+                userDoc = new User(userDoc, true)
+                userDoc = {
+                    _id: userDoc.data._id,
+                    username: userDoc.data.username,
+                    avatar: userDoc.avatar
+                }
+                resolve(userDoc)
+            } else {
+                reject()
+            }
+        }).catch(function () {
+            reject()
+        })
     })
 }
 
